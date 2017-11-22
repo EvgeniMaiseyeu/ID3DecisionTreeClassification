a4cd8549a83b5bbff3032d78a0a7d7898f82bc65
Evgeni Maiseyeu <emaiseyeu@my.bcit.ca>
2017-11-21 18:38:33 -0800

    initial class skeleton setup

---
+++ ID3DecisionTreeClassification/Feature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIAssignment5
+{
+    class Feature
+    {
+        public string name;
+        public string[] attributeList;
+
+        //In our examples attribute count should be size 2 always
+        Feature(string name, int attributeCount)
+        {
+            this.name = name;
+            attributeList = new string[attributeCount];
+        }
+    }
+}
---
+++ ID3DecisionTreeClassification/Individual.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIAssignment5
+{
+    class Individual
+    {
+        //name of the individual i.e. (trainEx1)
+        public string name;
+
+        //value of the individual in our example (survived or died)
+        public string value;
+
+
+    }
+}
